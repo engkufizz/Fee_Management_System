59f34778fc65da8569abfc02156c83c66391c03f
engkufizz <59971168+engkufizz@users.noreply.github.com>
2025-02-24 10:58:29 +0800

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('student_fees_db', 'root', 'ashne4834', {
+const sequelize = new Sequelize('student_fees_db', 'root', 'password', {
   host: 'localhost',
   dialect: 'mysql',
   logging: false,
